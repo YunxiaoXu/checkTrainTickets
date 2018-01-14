9d6e5c939fbee7b3d314d68f9e3e51dd526b6342
YunxiaoXu <xuyunxiao2001@gmail.com>
2018-01-14 16:29:04 +0800

    get data and modify the coding

--- checkTickets.py
+++ checkTickets.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #coding: utf-8
-"""
+u"""
 Train Tickets Checking Tool
 
 Usage:
@@ -24,6 +24,7 @@ Example:
 import sys
 import getopt
 import requests
+import platform
 
 dongche         = False
 gaotie          = False
@@ -33,9 +34,10 @@ zhida           = False
 from_station    = ""
 to_station      = ""
 date            = "" 
+new_coding      = "gbk" if platform.system()=="Windows" else "utf-8"
 
 def usage():
-    print(__doc__)
+    print(__doc__).encode(new_coding)
     sys.exit(0)
 
 def getInfo():
@@ -75,24 +77,44 @@ def getInfo():
         elif o in ("-Z", "--Direct"):
             zhida = True
         elif o in ("-f", "--from"):
-            from_station = a
+            from_station = a.decode(new_coding)
         elif o in ("-t", "--to"):
-            to_station = a
+            to_station = a.decode(new_coding)
         elif o in ("-d", "--date"):
-            date = a
+            date = a.decode(new_coding)
         else:
             assert False, "Unhandled Option"
 
     if not (dongche or gaotie or tekuai or kuaisu or zhida):
         dongche = gaotie = tekuai = kuaisu = zhida = True
 
+def getData():
+    global from_station
+    global to_station
+    global date
+
+    url_template = (
+        "https://kyfw.12306.cn/otn/leftTicket/queryZ?"
+        "leftTicketDTO.train_date={date}&"
+        "leftTicketDTO.from_station={from_station}&"
+        "leftTicketDTO.to_station={to_station}&"
+        "purpose_codes=ADULT"
+    )
+
+    request_url = url_template.format(from_station=from_station, to_station=to_station, date=date)
+
+    data = requests.get(request_url)
+    
+    print data.text.encode(new_coding)
+
 if __name__ == "__main__":
     getInfo()
-    print "动车：", dongche
-    print "高铁：", gaotie
-    print "特快：", tekuai
-    print "快速：", kuaisu
-    print "直达：", zhida
-    print "出发：", from_station
-    print "抵达：", to_station
-    print "日期：", date
+    print u"动车：{}".format(dongche).encode(new_coding)
+    print u"高铁：{}".format(gaotie).encode(new_coding)
+    print u"特快：{}".format(tekuai).encode(new_coding)
+    print u"快速：{}".format(kuaisu).encode(new_coding)
+    print u"直达：{}".format(zhida).encode(new_coding)
+    print u"出发：{}".format(from_station).encode(new_coding)
+    print u"抵达：{}".format(to_station).encode(new_coding)
+    print u"日期：{}".format(date).encode(new_coding)
+    getData()
